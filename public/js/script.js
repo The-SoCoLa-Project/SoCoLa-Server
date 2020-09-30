6a49028076a98cd7514185efcfed61c2d1b8d95b
Nena Basina <basina@ics.forth.gr>
2020-09-30 15:45:30 +0300

    implemented changes to UI

--- public/js/script.js
+++ public/js/script.js
@@ -4,75 +4,23 @@
 
 // Onload, convert the .lp input file to text and display it inside component 
 function onLoad() {
-    var txt = document.getElementById('inputFileText')
-    // var file = new File([""], "../reasoner/reasonerInit.txt");
-    // // file reader
-    // const reader = new FileReader();
-    // reader.readAsText(file);
-    // // file reading started
-    // reader.addEventListener('loadstart', function() {
-	//     console.log('File reading started');
-    // });
-    // // file reading finished successfully
-    // reader.addEventListener('load', function(e) {
-    //     txt.setAttribute('data', e.target.result)
-    //     var test = document.getElementById('resultsBox')
-    //     test.innerText = e.target.result
-    // })
-    // // file reading failed
-	// reader.addEventListener('error', function() {
-	//     alert('Error : Failed to read file');
-    // });
+    var txt = document.getElementById('visionFileText')
     txt.setAttribute('data', '../reasoner/visionInput.lp.txt')
     document.getElementById('interactionMsg').style.visibility = "hidden"
 }
 
-// const func = () => fetch(`https://localhost:80/api/reasoner/`, {
-//     method: "GET",
-//     headers: {
-//         "Content-Type": "text/plain"
-//     }
-// });
+// Make a GET request to the server using jquery
+const execJar = () => $.get("./api/reasoner/");
 
-const func = () => $.get("./api/reasoner/");
-
-// what happens when the Run Clingo btn is clicked
+// what happens when the Run Reasoner btn is clicked
 document.getElementById('runClingo').addEventListener('click', function(e) {
-    //test
-    var resultsBox = document.getElementById('resultsBox');
-    resultsBox.innerHTML = "run clingo button clicked";
-
-    var outputFile = document.getElementById('outputFileText');
-    outputFile.setAttribute('data', '/reasoner/reasonerOutput.txt');
-    func();
+    execJar();
+    // first run the jar, then load the file
+    var reasonerResult = document.getElementById('reasonerFileText');
+    reasonerResult.setAttribute('data', '/reasoner/reasonerOutput.txt');
     var msg = document.getElementById('interactionMsg');
     msg.style.visibility = "visible";
     msg.textContent = "Clingo has run successfully!";
     msg.style.color = "#5DA85D"
 })
 
-// what happens when the Edit File btn is clicked
-document.getElementById('editFile').addEventListener('click', function(e) {
-    //test
-    var resultsBox = document.getElementById('resultsBox');
-    resultsBox.innerHTML = "edit file button clicked";
-
-    var msg = document.getElementById('interactionMsg');
-    msg.style.visibility = "visible";
-    // msg.textContent = "Clingo has run successfully!";
-    // msg.style.color = "#5DA85D"
-})
-
-///////////////////////////////////////////////////////////////////////////////////////////
-// lower half - chatbot
-//
-// what happens when the OK btn in the chatbot area is clicked
-document.getElementById('ok').addEventListener('click', function() {
-    var inputTxt = document.getElementById('userInput').value
-    var reply = document.getElementById('botReply')
-    if (inputTxt == "") {
-        reply.innerText = "No input!"
-    } else {
-        reply.innerText = "You told me: " + inputTxt
-    }
-})
